49e33a1e8cdeb51f0b3aeee8fd824b185ae0ffaa
Hunny Chaurasia <chaurasiahunny2005@gmail.com>
2026-09-06 14:22:49 +0000

    Fix favicon asset bundling

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Dynamically set favicon via Vite bundle
+import favicon from './assets/favicon-full-logo-circular.png'
+
+const link = (document.querySelector("link[rel*='icon']") || document.createElement('link')) as HTMLLinkElement
+link.type = 'image/png'
+link.rel = 'icon'
+link.href = favicon
+document.getElementsByTagName('head')[0].appendChild(link)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
